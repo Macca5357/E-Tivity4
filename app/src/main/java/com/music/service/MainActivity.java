1b9cfe77ad67c15840b493a6a1970fd6fd67807f
Macca5357 <19237766@studentmail.ul.ie>
2023-04-11 17:23:43 +0100

    ConnectUI

--- app/src/main/java/com/music/service/MainActivity.java
+++ app/src/main/java/com/music/service/MainActivity.java
@@ -6,8 +6,12 @@ package com.music.service;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonStartService,buttonStopService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
